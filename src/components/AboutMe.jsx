62a025bc17fd2b10f6b05338c715069923e7c160
sharp <dylan.mark.sharp@gmail.com>
2022-12-09 06:25:22 +0200

    Updated about me.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,7 @@ const AboutMe = () => <>
     <p className="text-gray-600">Mechanical Engineer turned Software Engineer.</p>
     <p className="text-gray-600">I'm deep down the Bitcoin rabbit hole.</p>
     <p className="text-gray-600"> I love people and I love learning.</p>
-    <p className="text-gray-600 mb-5">I'm 33, a dad and married.</p>
+    <p className="text-gray-600 mb-5">33. Dad. Married.</p>
     <div className="flex flex-col center">
         <a href="mailto:dylan@dmsapps.co.za" target="_blank" className="mail-link social-link-hover">
             <i className='bx bx-at text-xl'></i>
