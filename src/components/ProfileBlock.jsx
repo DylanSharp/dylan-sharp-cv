1cfde9e0b27934475a665847760cefda7dac064d
sharp <dylan.mark.sharp@gmail.com>
2022-12-09 06:50:24 +0200

    Updated color scheme to oranges.

--- src/components/ProfileBlock.jsx
+++ src/components/ProfileBlock.jsx
@@ -13,7 +13,7 @@ const ProfileBlock = () => {
 
     return (
         <div className="shadow rounded-xl overflow-hidden">
-            <div className="h-32 bg-gradient-to-l from-orange-300 to-fuchsia-500"></div>
+            <div className="h-32 bg-gradient-to-tr from-yellow-400 to-amber-600"></div>
             <div className="pt-14 p-7 bg-white relative">
                 <span className="status-badge bg-green-200 text-gray-900">Available</span>
                 <a href="/"><img src={profilePhoto} alt="Avatar" className="user-photo"/></a>
